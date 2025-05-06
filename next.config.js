115366b61c431e7fb8cd0a5d98e8eba0a172ab0b
karthik <karthik.bhfconstructions@gmail.com>
2025-05-06 14:53:00 +0530

    fixed navbar

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-// const nextConfig = {
-//     output: 'export',
-//   }
+const nextConfig = {
+    output: 'export',
+  }
    
-//   module.exports = nextConfig
+  module.exports = nextConfig
